6e7a3c36562c10857874d67ecffc5622715b035d
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-07-22 21:09:01 -0600

    weshnet ios integration

--- packages/context/WalletsProvider/adena.ts
+++ packages/context/WalletsProvider/adena.ts
@@ -51,8 +51,8 @@ export const useAdena: () => UseAdenaResult = () => {
         setReady(true);
       }
     };
-    window.addEventListener("load", handleLoad);
-    return () => window.removeEventListener("load", handleLoad);
+    window?.addEventListener?.("load", handleLoad);
+    return () => window?.removeEventListener?.("load", handleLoad);
   }, []);
 
   useEffect(() => {
