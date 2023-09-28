59f6df706e56078c05efee48eed47a437e8c4e02
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-09-28 12:53:11 -0600

    document picker for web

--- packages/utils/waveform/waveform.ts
+++ packages/utils/waveform/waveform.ts
@@ -2,7 +2,7 @@ export const generateWaveForm = async (audio: any) => {
   return [];
 };
 
-export const getAudioData = async (audio: any) => {
+export const getAudioData = async (audio: ArrayBuffer) => {
   return {
     duration: 0,
     waveform: [],
